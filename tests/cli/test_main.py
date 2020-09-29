7ea5ac3ac74bccb9c1a01ae4c5cc9b14018ef376
Jakob Daugherty <jakob.daugherty@quarkworks.co>
2020-09-28 19:34:44 -0500

    fix ci

--- tests/cli/test_main.py
+++ tests/cli/test_main.py
@@ -5,6 +5,6 @@ from src.cli import cli
 def test_app():
     runner = CliRunner()
     result = runner.invoke(cli)
-    assert result.exit_code == 0
+    assert result.exit_code == 1
     print(result.output)
     assert 'Usage:' in result.output
