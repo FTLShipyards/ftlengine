dab03eb467d3324dbb9e984c24f4d0550190525e
Jakob Daugherty <jakob.daugherty@quarkworks.co>
2020-10-01 18:03:32 -0500

    [PATCH] Fix the docs again

--- docs/conf.py
+++ docs/conf.py
@@ -53,3 +53,10 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- START FTLengine specific stuff ------------------------------------------
+
+# Set the master document
+master_doc = 'index'
+
+# -- EOF ------
